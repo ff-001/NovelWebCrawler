9cece944fb53c2f75ba2e3c1050b7f157d7c9300
HuwGames Studio <siliwang.huw@gmail.com>
2020-02-28 17:55:30 -0800

    Parse Chapter Link
    Parsed Chapter URL to a list.

--- downloader.py
+++ downloader.py
@@ -3,30 +3,28 @@ import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-url='https://www.kanunu8.com/book4/10509/'
-txt=requests.get(url).content.decode('gbk') 
-m1=re.compile(r'<td colspan="4" align="center"><strong>(.+)</strong>')
+basedurl = 'http://novelfull.com'
+url='http://novelfull.com/index.php/hidden-marriage.html?page=1&per-page=50'
+#url = 'https://www.kanunu8.com/book4/10509/'
+txt=requests.get(url).content
+m1=re.compile(r'<span class="glyphicon glyphicon-certificate"></span>(.+)<span')
 #print(m1.findall(txt))
-m2=re.compile(r'<td( width="25%")?><a href="(.+\.html)">(.+)</a></td>')
-#print(m2.findall(txt))
-raw=m2.findall(txt) 
-sanguo=[]
-for i in raw:
-    sanguo.append([i[2],url+i[1]]) 
 
-#print(sanguo)
-
+raw = m1.findall(txt) 
+m2 = re.compile(r'<a href="(.+)" title="(.+)">')
 m3=re.compile(r'<p>(.+)</p>',re.S)  
 m4=re.compile(r'<br />')          
 m5=re.compile(r'&nbsp;&nbsp;&nbsp;&nbsp;')
+chapter = []
 with open('HW.txt','a') as f:
-    for i in sanguo:
-        i_url=i[1]  
-        print("download----->",i[0])   
-        r_nr=requests.get(i_url).content.decode('gbk')
+	for i in raw:	
+		item = m2.findall(i)
+		i_url=basedurl+item[0][0]  
+        print(i_url)   
+        r_nr=requests.get(i_url).content
         n_nr=m3.findall(r_nr)
         #print(n_nr)
         n=m4.sub('',n_nr[0]) 
         n2=m5.sub('',n)
-        f.write('\n'+i[0]+'\n') 
+        f.write('\n'+item[0][1]+'\n') 
         f.write(n2)
